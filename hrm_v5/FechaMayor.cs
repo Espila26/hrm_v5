dc4ff906ec65fb5b6f6b1f724fc67eef9df731c1
Marco <Marco@MARCO>
2017-02-27 14:04:26 -0600

    Estilo para el formulario de crear amonestaciones en acciones de personal. Clase para la validacion de las fechas (Saber si la fecha inicial es menor que la final)

---
+++ hrm_v5/FechaMayor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace hrm_v5
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FechaMayor : ValidationAttribute
+    {
+        public FechaMayor(string dateToCompareToFieldName)
+        {
+            DateToCompareToFieldName = dateToCompareToFieldName;
+        }
+
+        public string DateToCompareToFieldName { get; private set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            DateTime earlierDate = (DateTime)value;
+            DateTime laterDate = (DateTime)validationContext.ObjectType.GetProperty(DateToCompareToFieldName).GetValue(validationContext.ObjectInstance, null);
+
+            if (laterDate < earlierDate)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("La Fecha Final debe ser posterior a la inicial.");
+            }
+
+        }
+    }
+}
